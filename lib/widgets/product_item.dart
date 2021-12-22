79fb4372643e1a9cc2546170ec8131d1e2cae0e6
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-23 00:39:13 +0300

    products view updated

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -10,7 +10,29 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(imageUrl),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(
+            Icons.favorite,
+          ),
+          onPressed: () {},
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.shopping_cart,
+          ),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
