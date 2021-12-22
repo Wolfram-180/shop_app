6c4ecb928ff66118cb3287724938ddb3df05a685
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-22 18:36:29 +0300

    images updated

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -12,7 +12,7 @@ class ProductsOverviewScreen extends StatelessWidget {
       description: 'A red shirt - it is pretty red!',
       price: 29.99,
       imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+          'https://shmultu.ru/wp-content/uploads/2021/12/file_177022Dec2021082405090232.jpg',
     ),
     Product(
       id: 'p2',
@@ -20,7 +20,7 @@ class ProductsOverviewScreen extends StatelessWidget {
       description: 'A nice pair of trousers.',
       price: 59.99,
       imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+          'https://shmultu.ru/wp-content/uploads/2021/12/file_176121Dec2021070345541816.jpg',
     ),
     Product(
       id: 'p3',
@@ -28,7 +28,7 @@ class ProductsOverviewScreen extends StatelessWidget {
       description: 'Warm and cozy - exactly what you need for the winter.',
       price: 19.99,
       imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+          'https://shmultu.ru/wp-content/uploads/2021/12/file_175921Dec2021052347931678.jpg',
     ),
     Product(
       id: 'p4',
@@ -36,7 +36,7 @@ class ProductsOverviewScreen extends StatelessWidget {
       description: 'Prepare any meal you want.',
       price: 49.99,
       imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+          'https://shmultu.ru/wp-content/uploads/2021/12/file_174118Dec2021003958419346.jpg',
     ),
   ];
 
