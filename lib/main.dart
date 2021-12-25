585a98d6773b20544df64ca1e65ad1fe3bf1153d
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-25 22:50:49 +0300

    Notifier updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 
 import 'screens/product_detail_screen.dart';
-
-//import 'screens/products_overview_screen.dart';
-//import 'package:provider/provider.dart';
+import 'screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
-          secondary: Colors.deepOrange,
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
+            secondary: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -10,7 +10,7 @@ class Products with ChangeNotifier {
   }
 
   void addProduct() {
-    _items.add(value);
+    //_items.add(value);
     notifyListeners();
   }
 }
