c9011801d277240b324d20847e047c2a53186f76
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-30 04:53:59 +0300

    Drawer added to screens

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
 
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
@@ -17,6 +18,7 @@ class OrdersScreen extends StatelessWidget {
           'Your orders',
         ),
       ),
+      drawer: AppDrawer(),
       body: ListView.builder(
         itemCount: orderData.orders.length,
         itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/badge.dart';
 
 import '../widgets/products_grid.dart';
@@ -66,6 +67,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
