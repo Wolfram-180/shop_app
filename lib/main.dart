86f2d7ad0e37b3e201a22e809cacae1104cfb06d
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-30 04:51:55 +0300

    Drawer created

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/screens/orders_screen.dart';
 
 import 'providers/orders.dart';
 import 'screens/cart_screen.dart';
@@ -38,6 +39,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
         },
       ),
     );
