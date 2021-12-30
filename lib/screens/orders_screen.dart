3ecce1e2e4a3f1886684a42a983ffbd2fc0159d3
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-30 04:41:58 +0300

    Order_item widget created

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Your orders',
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+      ),
+    );
   }
 }
