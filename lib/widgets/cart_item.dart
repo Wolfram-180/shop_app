6339b6f35993c7b75dd22957f9b6a5191fe55daf
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-31 15:37:59 +0300

    AlertDialog added to Dismiss

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -33,6 +33,30 @@ class CartItem extends StatelessWidget {
         padding: EdgeInsets.only(right: 20),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item from the cart?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+                child: Text('No'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        );
+        return Future.value(true);
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(
           context,
