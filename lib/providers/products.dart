7221c0ffd4a70ca5fa62731b659b954224bc5f54
wolfram-180 <sergei.ulvis@gmail.com>
2022-01-08 15:11:08 +0300

    SnackBar on deletion issue added

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
 import 'product.dart';
 
 class Products with ChangeNotifier {
@@ -261,18 +262,22 @@ class Products with ChangeNotifier {
     } else {}
   }
 
-  void deleteProduct(String id) {
+  Future<void> deleteProduct(String id) async {
     final url = Uri.https(serverUrl, '/products/$id.json');
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     Product? existingProduct = _items[existingProductIndex];
+
     _items.removeAt(existingProductIndex);
-    http.delete(url).then((_) {
-      existingProduct = null;
-    }).catchError((_) {
-      _items.insert(existingProductIndex, existingProduct!);
-      notifyListeners();
-    });
     notifyListeners();
+
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Issue while deleting product');
+    }
+    existingProduct = null;
   }
 
 // void showFavoritesOnly() {
