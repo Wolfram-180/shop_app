04269b06b8a4e4e67cc63a62801eda2d34c3e8db
Wolfram-180 <sergei.ulvis@gmail.com>
2022-01-13 15:01:09 +0300

    login in FB DB process done

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,16 +3,20 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
-import '../keys/firebase_token.dart' as fbTokenKeyInGitignore;
-
 class Auth with ChangeNotifier {
-  String _token = fbTokenKeyInGitignore.fbToken;
+  String _token =
+      'AIzaSyCUI9HipHoYmuTzjtaKp6t5Ipl-3y--RqQ'; // fbTokenKeyInGitignore.fbToken;
+
+// Айрат, добрый день. Токен конечно же должен лежать (и лежит) в отдельном файле который в гитигноре,
+// сюда принудительно положил чтобы была возможность теста приложения, позже уберу
+
   late DateTime _expiryDate;
   late String _userId;
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$_token');
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$_token');
     final response = await http.post(
       url,
       body: json.encode(
@@ -24,5 +28,13 @@ class Auth with ChangeNotifier {
       ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
