48ef296ab2fc5c8535d7ab3eb21752cb470983cf
Wolfram-180 <sergei.ulvis@gmail.com>
2022-01-18 13:14:52 +0300

    Hero animation added

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -22,10 +22,14 @@ class ProductItem extends StatelessWidget {
                 arguments: product.id,
               );
             },
-            child: FadeInImage(
-              placeholder: AssetImage('assets/images/product-placeholder.png'),
-              image: NetworkImage(product.imageUrl),
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder:
+                    AssetImage('assets/images/product-placeholder.png'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
             )
             // Image.network(
             //   product.imageUrl,
