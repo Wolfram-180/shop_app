67c64fc67a795f7cc2a77bf375fec8f1e7730e25
Wolfram-180 <sergei.ulvis@gmail.com>
2022-03-27 16:54:52 +0300

    Grid changed to 3 columns

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -18,7 +18,7 @@ class ProductsGrid extends StatelessWidget {
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
+        crossAxisCount: 3,
         childAspectRatio: 3 / 2,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
